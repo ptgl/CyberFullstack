ba7fc5709e961363291840a8574c66f845e4b60c
glinh <pgialinh.iuetv@gmail.com>
2019-06-30 17:09:58 +0700

    initial project

---
+++ master/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "./css/index.css";
+
+function App() {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
+
+export default App;
