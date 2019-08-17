a2bbe5f1a94ee3962ac1198eb383c24d6f92a795
ptgl <pgialinh.iuetv@gmail.com>
2019-08-17 08:11:25 +0700

    init

---
+++ master/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const userRouter = require("./routes/api/user");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/fs05-xedike", { useNewUrlParser: true, createIndexes: true})
+  .then(() => console.log("Connected successfully"))
+  .catch(console.log);
+
+const port = process.env.PORT || 5000;
+// cmd: $env:PORT = 1234
+
+const app = express();
+
+//middleware parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+//middleware static file
+app.use("/upload/avatars", express.static('./upload/avatars'))
+
+//middleware route handler
+app.use("/api/users", userRouter);
+
+
+// app.use(
+//   (req, res, next) => {
+//     console.log("middle ware 1");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("middle ware 2");
+//     // res.send("Fnish")
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("middle ware 3");
+//     next();
+//   }
+// );
+
+
+app.listen(port, () => console.log(`App is running on port ${port}`));
